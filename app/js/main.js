73144fe9a35f82017e485a56e549f64e1280efc5
thedean11 <edean1010@gmail.com>
2015-02-01 21:57:08 -0600

    added google maps, styled to be circular, added navigation sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,30 @@
-;(function(){
-  'use strict';
 
-  console.log('NSS Template Loaded');
-}());
+///////////////////////////////////////////////////////////////////
+///////////////////// Google Maps Initialize ///////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+var berlin = new google.maps.LatLng(52.520816, 13.410186);
+
+var markers = [];
+
+var map;
+
+function initialize() {
+  var mapOptions = {
+    zoom: 12,
+    center: berlin,
+    disableDefaultUI: true,
+    scrollwheel: false,
+    navigationControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    draggable: false,
+    zindex: 99,
+    transparency: 1
+  };
+
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+          mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
