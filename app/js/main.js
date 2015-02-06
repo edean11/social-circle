fb8e992aee7da9bc09b2a58e8e00244149b71def
thedean11 <edean1010@gmail.com>
2015-02-05 19:01:26 -0600

    added buttons for circle additions in the footer

--- app/js/main.js
+++ app/js/main.js
@@ -38,6 +38,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
 // DOM Element Grab
 
 var $footer = $('.footer-container');
+var $circleAppender = $('.footerCircle-container')
 //var $addGroupButton = $('#addGroup');
 var $castMessageButton = $('#castMessageButton');
 var $searchButton = $('#searchButton');
@@ -51,8 +52,9 @@ var $settingsButton = $('#settingsButton');
 
 $castMessageButton.on("click", function(){
 
+    var castMessageOverlayHeight = $('.castMessageOverlay').height();
     var editOverlaySelect = $('.editOverlay');
-    editOverlaySelect.css('height', '440px');
+    editOverlaySelect.css('height', '100%');
     var castMessageOverlay = $('.castMessageOverlay');
     castMessageOverlay.css('display', 'inline-block');
     $castMessageButton.css('z-index', '10');
@@ -62,47 +64,50 @@ $castMessageButton.on("click", function(){
 $searchButton.on("click", function(){
 
     var editOverlaySelect = $('.editOverlay');
-    editOverlaySelect.css('height', '440px');
+    editOverlaySelect.css('height', '100%');
+    var searchOverlay = $('.searchOverlay');
+    searchOverlay.css('display', 'inline-block');
+    $searchButton.css('z-index', '10');
 
 });
 
 $notificationsButton.on("click", function(){
 
     var editOverlaySelect = $('.editOverlay');
-    editOverlaySelect.css('height', '440px');
+    editOverlaySelect.css('height', '100%');
+    var notificationsOverlay = $('.notificationsOverlay');
+    notificationsOverlay.css('display', 'inline-block');
+    $notificationsButton.css('z-index', '10');
 
 });
 
 $settingsButton.on("click", function(){
 
     var editOverlaySelect = $('.editOverlay');
-    editOverlaySelect.css('height', '440px');
+    editOverlaySelect.css('height', '100%');
+    var settingsOverlay = $('.settingsOverlay');
+    settingsOverlay.css('display', 'inline-block');
+    $settingsButton.css('z-index', '10');
 
 });
 
 
-///////////////////////////////
-////// Input Functions ////////
-///////////////////////////////
-
-function castMessageOverlay(appendTo) {
-  var castTitle = $('<input placeholder="Title"></input>');
-  var castMessage = $('<textarea placeholder="Message"></textarea>');
-  var castRadius
-}
+////////////////////////////////////////
+////// Add/Remove Circle Functions ////////
+/////////////////////////////////////////
 
 
-// Add Group Function
+var windowWidth = $( window ).width();
+var circleContainerWidth = '75px';
+var circleContainerHeight = '75px';
 
-function addGroup(){
+// Add Circle Function
+
+function addCircle(){
     var existingDivs = [];
   _.forEach($footer.children(), function(c){
     existingDivs.push(c);
   });
-
-  var windowWidth = $( window ).width();
-  var circleContainerWidth = windowWidth/6;
-  var circleContainerHeight = circleContainerWidth;
 
   var $circleContainer = $('<div class="circleContainer"></div>')
     $circleContainer.css("display", "inline-block");
@@ -112,6 +117,31 @@ function addGroup(){
     $circleContainer.css("margin", "5px");
     $circleContainer.css("border", "4px solid black");
     $circleContainer.css("border-radius", "25%");
-  $footer.append($circleContainer);
+  $circleAppender.append($circleContainer);
 }
+
+//These click events and buttons to click will be unnecessary once the back end is configured.  The back end should
+//read/write the followed circles (groups) tied to the specific user from the database.  This is then updated every so often.
+//When a user clicks any of these circles, the map above will update to only show messages corersponding to that group(i.e. this is a quick filter function)
+
+// Add Circle Click Event
+
+var $addCircleButton = $('#addCircleButton');
+$addCircleButton.on('click', function(){
+  addCircle();
+});
+
+// Remove Circle Click Event
+
+var $removeCircleButton = $('#removeCircleButton');
+
+$removeCircleButton.on('click', function(){
+  $circleAppender.on('click', '.circleContainer', function(){
+    $(this).remove();
+    $circleAppender.off('click');
+  });
+
+});
+
+
 
